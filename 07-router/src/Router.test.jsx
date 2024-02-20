72b8237375c7b74f28987dfcfb4df846b640a593
arromero4 <arromero0408@gmail.com>
2024-02-20 03:29:06 -0600

    added: More test and utils

---
+++ 07-router/src/Router.test.jsx
@@ -0,0 +1,71 @@
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import { render, screen, cleanup, fireEvent, waitFor } from "@testing-library/react";
+import { Router } from "./Router.jsx";
+import { getCurrentPath } from "./utils.js";
+import { Route } from "./Route.jsx";
+import { Link } from "./Link.jsx";
+
+vi.mock('./utils', () => ({
+    getCurrentPath: vi.fn()
+}))
+
+describe('Router',  () => {
+    beforeEach(() => {
+        cleanup()
+        vi.clearAllMocks()
+    })
+    it('should work', () => {
+        render(<Router routes={[]} />)
+        expect(true).toBeTruthy();
+    })
+
+    it('should render 404 if no routes match', () => {
+        render(<Router routes={[]}  defaultComponent={()=> <h1>404</h1>}/>)
+        expect(screen.getByText('404')).toBeTruthy()
+    })
+
+    it('should render the component of the first route that matches', () => {
+        getCurrentPath.mockReturnValue('/about')
+        const routes = [
+            {
+                path: '/',
+                Component: () => <h1>Home</h1>
+            },
+            {
+                path: '/about',
+                Component: () => <h1>About</h1>
+            }
+        ]
+        render(<Router routes={routes} />)
+        expect(screen.getByText('About')).toBeTruthy()
+    })
+
+    it('should navigate using Links', async () => {
+        getCurrentPath.mockReturnValueOnce('/')
+        render(
+            <Router>
+                <Route 
+                path='/' Component={() => {
+                    return (
+                        <>
+                            <h1>Home</h1>
+                            <Link to='/about'>Go to About</Link>
+                        </>
+                    )
+                }} />
+                <Route path='/about' Component={() => <h1>About</h1>} />
+            </Router>
+        )
+
+        const button = screen.getByText(/Go to About/)
+        fireEvent.click(button)
+
+        await waitFor(() => screen.findByText('About'))
+
+        const aboutTitle = await screen.findByText('About')
+
+        expect(aboutTitle).toBeTruthy()
+    })
+
+
+})
