82cf6cf87b46f991b6c652356436488520529da5
arromero4 <arromero0408@gmail.com>
2024-03-03 00:44:11 -0600

    Updated with Hooks and Icons

---
+++ 09-google-translate-clone/src/App.tsx
@@ -0,0 +1,39 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import { useStore } from './hooks/useStore';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { AUTO_LANGUAGE } from './constants';
+import { ArrowsIcon } from './components/Icons';
+
+function App() {
+  const { fromLanguage, toLanguage, interchangeLanguages } = useStore()
+  return (
+    <Container fluid>
+      <h1>Google Translate</h1>
+
+      <Row>
+        <Col>
+          <h2>From</h2>
+          {fromLanguage}
+        </Col>
+
+        <Col>
+          <Button
+          variant='link'
+          disabled={fromLanguage === AUTO_LANGUAGE} 
+          onClick={interchangeLanguages}>
+            <ArrowsIcon/>
+          </Button>
+        </Col>
+
+        <Col>
+          <h2>To</h2>
+          {toLanguage}
+        </Col>
+      </Row>
+
+    </Container>
+  )
+}
+
+export default App
